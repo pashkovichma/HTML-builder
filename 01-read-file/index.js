387f1bd84843f9342807f19ac12a6f68149daa3e
Maria <pashkovichma@gmail.com>
2022-05-22 15:07:54 +0400

    feat: add 01 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const rs = fs.createReadStream(path.join(__dirname, 'text.txt'));
+
+let data = '';
+
+rs.on('data', (chunk) => {
+    data += chunk.toString();
+});
+
+rs.on('end', () => {
+    console.log(data);
+});
