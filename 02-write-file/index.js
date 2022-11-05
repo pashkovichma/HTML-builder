7f2f7a53280b19c1daa84a9b473b88767ae5060d
Maria <pashkovichma@gmail.com>
2022-11-05 09:16:32 +0300

    refactor: 01-read-fil

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,38 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
+const { stdout, stdin } = require('process');
+
+const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Введите текст, пожалуйста\n');
+
+stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+        process.exit();
+    }
+    file.write(data, (error) => {
+        if (error) throw error;
+    });
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', (code) => {
+    if (code === 0) {
+        console.log('Пока - пока');
+    } else {
+        rl.write(`Ой, ошибка\n`);
+    }
+});
+
+/*const process = require('process');
+const fs = require('fs');
+const path = require('path');
 const rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const file = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
 rl.write('Введите текст, пожалуйста\n');
 
@@ -13,7 +40,7 @@ rl.on('line', (text) => {
     if (text.trim() === 'exit') {
         process.exit();
     }
-    file.write(text.toString(), (error) => {
+    file.write(text, (error) => {
         if (error) throw error;
     });
 });
@@ -29,17 +56,4 @@ process.on('exit', (code) => {
         rl.write(`Ой, ошибка\n`);
     }
 });
-
-/*
-const fs = require('fs');
-const readline = require('readline');
-const path = require('path');
-
-const rl readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-const pathToFile = path.join('02-write-file', 'text.txt');
-
-const fileText = fs.createWriteStream(pathToFile);
+*/
