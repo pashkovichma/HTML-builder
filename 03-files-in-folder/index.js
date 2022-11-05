dea70549156006ab16cb5134c0a847a06dbea8d6
Maria <pashkovichma@gmail.com>
2022-11-05 12:42:24 +0300

    refactor: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,19 +3,18 @@ const path = require('path');
 const folderPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(folderPath, {withFileTypes: true }, (err, files) => {
-  if (err) throw err;
+    if (err) throw err;
 
     for (let file of files) {
-        const name = file.name.split('.').slice(0, -1).join('.');
-        const filePath = path.join(__dirname, 'secret-folder', file.name);
-        const extension = path.extname(filePath).slice(1);
-
-      fs.stat(filePath, (err, file) => {
-        if (err) throw err;
-
         if (file.isFile()) {
-          console.log( `${name} - ${extension} - ${file.size}B`);
+          const name = file.name.split('.').slice(0, -1).join('.');
+          const filePath = path.join(__dirname, 'secret-folder', file.name);
+          const extension = path.extname(filePath).slice(1);
+
+          fs.stat(filePath, (err, file) => {
+            if (err) throw err;
+            console.log( `${name} - ${extension} - ${file.size}B`);
+          })
         }
-      })
     }
-  });
+});
